644659c0bf731641e5e882c94dd5043b510dbef9
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-08-14 18:31:04 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    return <h1>Hello, world!!</h1>;
   }
-
 }
 
 export default App;
